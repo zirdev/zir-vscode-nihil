240ab3be8c044f1534fba3ff24fc094517a6d391
Zireael <zirdev@gmail.com>
2019-08-29 00:34:28 +0100

    minor update

--- .samples/JS/imports.js
+++ .samples/JS/imports.js
@@ -2,7 +2,7 @@ import { React } from "react";
 import { ReactDOM } from 'react-dom';
 import App from './components.app';
 import * as something from './something.test';
-const path = require("path"); // NodeJS test
+const path = require("path");
 
 ReactDOM.render(
     <App/>,
