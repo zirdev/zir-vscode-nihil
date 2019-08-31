a2b3eba4f71cf8fa8da15e6ff207e07eb26b7a94
Zireael <zirdev@gmail.com>
2019-08-31 13:42:22 +0100

    update screenshots

--- .samples/JS/regex.js
+++ .samples/JS/regex.js
@@ -3,52 +3,7 @@ const one = '123 s'.match(/^(\d{3})(?:\s)(\w+)$/)
 const re = /(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/;
 let animalCount = /\b\d+ (pig|cow|chicken)s?\b/;
 
-module.exports = [
-    // {
-    //     "scope": [
-    //     ],
-    //     "settings": { "foreground": "#9cdcfe", },
-    // },
-    {
-        "scope": [
-            "keyword.operator.quantifier.regexp",
-            "keyword.operator.quantifier.regexp.js",
-        ],
-        "settings": { "foreground": "#b5cea8", },
-    },
-    {
-        "scope": [
-            "string.regexp",
-            "constant.regexp",
-        ],
-        "settings": { "foreground": "#d16969", },
-    },
-    {
-        "scope": [
-            "constant.character.escape.backslash.regexp",
-        ],
-        "settings": { "foreground": "#d7ba7d", },
-    },
-    {
-        "scope": [
-            "constant.other.character-class.regexp",
-        ],
-        "settings": { "foreground": "#4ec9b0", },
-    },
-    {
-        "scope": [
-            "keyword.control.anchor.regexp",
-            "constant.other.character-class.regexp",
-        ],
-        "settings": { "foreground": "#569cd6", },
-    },
-    {
-        "scope": [
-            "punctuation.definition.group.regexp",
-            "punctuation.definition.character-class.regexp",
-        ],
-        "settings": { "foreground": "#d4d4d4", },
-    },
-];
+const sanitizeSvg = /(<|%3C)\s*?script[\s\S]*?(>|%3E)[\s\S]*?(<|%3C)\s*?(\/|%2F)\s*?script[\s\S]*?(>|%3E)/gims;
 
-one; re; animalCount
+
+one; re; animalCount; sanitizestring;
